537026aa2f7b07d94a8d7b4db005b5b2901c7b59
kedar-007 <cakedar7390@gmail.com>
2025-12-29 13:09:29 +0530

    Added the widget loading with the session and organization

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -16,6 +16,8 @@ import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { platform } from "os";
 import { Doc } from "@workspace/backend/_generated/dataModel";
+import { useAtomValue, useSetAtom } from "jotai";
+import { contactSessionIdAtomFamily, organizationIdAtom } from "../../atoms/widget-atoms";
 
 const formSchema = z.object({
     name: z.string().min(1, "Name is required"),
@@ -26,6 +28,10 @@ const formSchema = z.object({
 const orgnizationId = "123";
 
 export const WidgetAuthScreen = () => {
+    const organizationId = useAtomValue(organizationIdAtom);
+    const setContactSessionId = useSetAtom(
+        contactSessionIdAtomFamily(organizationId || "")
+    )
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -61,7 +67,7 @@ export const WidgetAuthScreen = () => {
             metadata,
         })
 
-        console.log("ContactSession Id",contactSessionId);
+        setContactSessionId(contactSessionId);
     };
 
     return (
