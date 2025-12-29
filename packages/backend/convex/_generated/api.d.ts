1064517de9d207bdb095b4ab1d4626964f45f2c1
kedar-007 <cakedar7390@gmail.com>
2025-12-29 13:17:50 +0530

    added back the contactSessions convex functions

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as public_contactSessions from "../public/contactSessions.js";
 import type * as public_organizations from "../public/organizations.js";
 import type * as users from "../users.js";
 
@@ -25,6 +26,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  "public/contactSessions": typeof public_contactSessions;
   "public/organizations": typeof public_organizations;
   users: typeof users;
 }>;
