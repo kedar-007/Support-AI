2078d3259e017f5910b67c68a6e51cf27a0ea0e8
kedar-007 <cakedar7390@gmail.com>
2026-01-06 16:00:47 +0530

    Added the inbox section in the dashboard also with the infinite scroll

--- packages/ui/src/components/resizable.tsx
+++ packages/ui/src/components/resizable.tsx
@@ -6,15 +6,16 @@ import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@workspace/ui/lib/utils"
 
-function ResizablePanelGroup({
+/* ---------------- Panel Group ---------------- */
+
+const ResizablePanelGroup = ({
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => {
   return (
     <ResizablePrimitive.PanelGroup
-      data-slot="resizable-panel-group"
       className={cn(
-        "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+        "flex h-full w-full min-w-0 data-[panel-group-direction=vertical]:flex-col",
         className
       )}
       {...props}
@@ -22,35 +23,47 @@ function ResizablePanelGroup({
   )
 }
 
-function ResizablePanel({
-  ...props
-}: React.ComponentProps<typeof ResizablePrimitive.Panel>) {
-  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />
+/* ---------------- Panel ---------------- */
+
+const ResizablePanel = (
+  props: React.ComponentProps<typeof ResizablePrimitive.Panel>
+) => {
+  return (
+    <ResizablePrimitive.Panel
+      className="min-w-0 min-h-0"
+      {...props}
+    />
+  )
 }
 
-function ResizableHandle({
+/* ---------------- Resize Handle ---------------- */
+
+const ResizableHandle = ({
   withHandle,
   className,
   ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
   withHandle?: boolean
-}) {
+}) => {
   return (
     <ResizablePrimitive.PanelResizeHandle
-      data-slot="resizable-handle"
       className={cn(
-        "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+        "relative flex w-1 bg-border hover:bg-border/80 transition-colors",
         className
       )}
       {...props}
     >
       {withHandle && (
-        <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
-          <GripVerticalIcon className="size-2.5" />
+        <div className="absolute inset-y-0 left-1/2 -translate-x-1/2 flex items-center">
+          <GripVerticalIcon className="h-4 w-4 text-muted-foreground" />
         </div>
       )}
     </ResizablePrimitive.PanelResizeHandle>
   )
 }
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
+export {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+}
